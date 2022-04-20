bf7ee6b83025a5ee51e2d3d97c6efe237df625f9
stfe30 <101103094+stfe30@users.noreply.github.com>
2022-04-20 13:40:55 +0200

    Create c5.c

---
+++ c5.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+  unsigned char var = 255;
+  printf("Wert =  %d\n", var);
+  var++;
+  printf("Wert =  %d\n", var);
+  printf("Wert =  %d\n", 10/var);
+  return 0;
+}
