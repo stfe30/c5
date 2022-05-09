f92da17515b62078f77d2ab7d969da02b04d564f
stfe30 <101103094+stfe30@users.noreply.github.com>
2022-05-09 08:58:39 +0200

    Update c5.c

--- c5.c
+++ c5.c
@@ -1,10 +1,10 @@
+// TEST-File: 
+
 #include <stdio.h>
 
 int main() {
   unsigned char var = 255;
-  printf("Wert =  %d\n", var);
   var++;
   printf("Wert =  %d\n", var);
-  printf("Wert =  %d\n", 10/var);
   return 0;
 }
